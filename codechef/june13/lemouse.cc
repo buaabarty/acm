decf65b3ac5857a15a4017f2cd18567e1c03e454
buaabarty <buaabarty@gmail.com>
2013-06-18 00:29:28 +0800

    2013-06-17

--- codechef/june13/lemouse.cc
+++ codechef/june13/lemouse.cc
@@ -34,7 +34,7 @@ template <typename T> void checkMax(T &a, const T &b) { if (b > a) a = b; }
 
 const int N = 100 + 9;
 
-int dat[N][N], pre[N][N], n, m, dp[N][N];
+int dat[N][N], n, m, dp[2][N][N];
 int step[5][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {0, 0}};
 
 int main (int argc, char *argv[]) {
@@ -43,24 +43,19 @@ int main (int argc, char *argv[]) {
     while (T--) {
         scanf("%d%d", &n, &m);
         memset(dat, 0, sizeof(dat));
-        memset(pre, 0, sizeof(pre));
         for (int i = 1; i <= n; ++i)
             for (int j = 1; j <= m; ++j)
-                scanf("%1d", &pre[i][j]);
-        for (int i = 1; i <= n; ++i)
-            for (int j = 1; j <= m; ++j)
-                for (int k = 0; k < 5; ++k) {
-                    int dx = i + step[k][0];
-                    int dy = j + step[k][1];
-                    if (dx < 1 || dy < 1 || dx > n || dy > m) continue;
-                    dat[i][j] += pre[dx][dy];
-                }
+                scanf("%1d", &dat[i][j]);
         memset(dp, 0x3f, sizeof(dp));
-        dp[1][1] = 0;
+        dp[0][0][1] = dp[1][1][0] = dat[1][1];
         for (int i = 1; i <= n; ++i)
-            for (int j = 1; j <= m; ++j)
-                checkMin(dp[i][j], min(dp[i-1][j]-pre[i-1][j]-pre[i][j], dp[i][j-1]-pre[i][j-1]-pre[i][j]) + dat[i][j]);
-        printf("%d\n", dp[n][m]);
+            for (int j = 1; j <= m; ++j) {
+                checkMin(dp[0][i][j], dp[1][i][j-1] + dat[i+1][j] + dat[i][j + 1]);
+                checkMin(dp[0][i][j], dp[0][i][j-1] + dat[i-1][j] + dat[i+1][j] + dat[i][j+1]);
+                checkMin(dp[1][i][j], dp[0][i-1][j] + dat[i+1][j] + dat[i][j+1]);
+                checkMin(dp[1][i][j], dp[1][i-1][j] + dat[i][j-1] + dat[i][j+1] + dat[i+1][j]);
+            }
+        printf("%d\n", min(dp[0][n][m], dp[1][n][m]));
     }
     return 0;
 }
